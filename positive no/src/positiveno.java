256f801c14f48b0d041d71c9d1112fa158c16b59
Vishal Pansare <51355863+vishalpansare5600@users.noreply.github.com>
2023-03-17 12:03:13 +0530

    Add files via upload

---
+++ positive no/src/positiveno.java
@@ -0,0 +1,19 @@
+
+public class positiveno 
+{
+
+	public static void main(String[]args) 
+	{
+		int no=Integer.parseInt(args[0]);
+		if(no >0)
+		{
+			System.out.println("given no is positive");
+			
+		}
+		else
+		{
+			System.out.println("given no is negative");
+		}
+	}
+
+}
